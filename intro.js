3a5171579ecb9350d4f0ee78973b3c0e2e4d5a4f
2435aman <dineshkumarr254@gmail.com>
2025-08-21 19:34:43 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Fronted Developer","Backed Developer","Data Science","Game Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backeDelay:1000,
+    loop:true
+})
